438e1f168798765f8c0f58f008fca25a4f13cb9b
iqbalpanduwinata19 <110536862+iqbalpanduwinata19@users.noreply.github.com>
2022-11-29 18:24:38 +0700

    Update v.2, kurang penyesuaian Warna

---
+++ script.js
@@ -0,0 +1,57 @@
+// Burger menus
+document.addEventListener('DOMContentLoaded', function () {
+    // open
+    const burger = document.querySelectorAll('.navbar-burger');
+    const menu = document.querySelectorAll('.navbar-menu');
+
+    if (burger.length && menu.length) {
+        for (var i = 0; i < burger.length; i++) {
+            burger[i].addEventListener('click', function () {
+                for (var j = 0; j < menu.length; j++) {
+                    menu[j].classList.toggle('hidden');
+                }
+            });
+        }
+    }
+
+    // close
+    const close = document.querySelectorAll('.navbar-close');
+    const backdrop = document.querySelectorAll('.navbar-backdrop');
+
+    if (close.length) {
+        for (var i = 0; i < close.length; i++) {
+            close[i].addEventListener('click', function () {
+                for (var j = 0; j < menu.length; j++) {
+                    menu[j].classList.toggle('hidden');
+                }
+            });
+        }
+    }
+
+    if (backdrop.length) {
+        for (var i = 0; i < backdrop.length; i++) {
+            backdrop[i].addEventListener('click', function () {
+                for (var j = 0; j < menu.length; j++) {
+                    menu[j].classList.toggle('hidden');
+                }
+            });
+        }
+    }
+});
+
+// Get the button
+const toTopButton = document.getElementById("to-top-button");
+
+// When the user scrolls down 200px from the top of the document, show the button
+window.onscroll = function () {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        toTopButton.classList.remove("hidden");
+    } else {
+        toTopButton.classList.add("hidden");
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function goToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
